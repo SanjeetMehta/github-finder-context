0af9b72af5880d32d849d08b72047acfb3619c17
Sanjeetkumar Mehta <sanjeetkmehta@gmail.com>
2020-07-14 11:20:14 +0530

    Moved get users to context

---
+++ src/components/users/Users.js
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types";
+import React, {useContext} from "react";
+import Spinner from "../layout/Spinner";
+import UserItems from "./UserItems";
+import GithubContext from "../../context/github/githubContext";
+
+const Users = () => {
+    const githubContext = useContext(GithubContext);
+    const {loading, users} = githubContext;
+    if (loading) {
+        return <Spinner />;
+    } else {
+        return (
+            <div style={userStyle}>
+                {users.map(user => (
+                    <UserItems key={user.id} user={user} />
+                ))}
+            </div>
+        );
+    }
+};
+
+const userStyle = {
+    display: "grid",
+    gridTemplateColumns: "repeat(3,1fr)",
+    gridGap: "1rem"
+};
+
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
+};
+export default Users;
